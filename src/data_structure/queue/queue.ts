4b346db489ae5f4e12991f852796e16488c52255
rufinatsoi <2770030+rufinn@users.noreply.github.com>
2022-11-19 23:28:42 +0000

    add queue structure

---
+++ src/data_structure/queue/queue.ts
@@ -0,0 +1,21 @@
+/**
+ * Write a queue structure with javascript.
+ * First In, First Out.
+ */
+class Queue {
+    values: any[];
+
+    constructor (values: any[] = []) {
+        this.values = values;
+    }
+
+    add (element: any) {
+        this.values.push(element);
+    }
+
+    remove() {
+        this.values.shift();
+    }
+}
+
+export default Queue;
